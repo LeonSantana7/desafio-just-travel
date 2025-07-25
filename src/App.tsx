56ba5fe9ff287afa91dcde33de1f97393c82746b
LeonSantana7 <leon.santana854@gmail.com>
2025-07-25 12:15:24 -0300

    feat: preparando o deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 
 import HomePage from "./pages/HomePage";
@@ -7,16 +7,15 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Header />
       <Container className="py-4">
-        {" "}
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/ticket/:id" element={<TicketDetailPage />} />
         </Routes>
       </Container>
-    </BrowserRouter>
+    </>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import { store } from "./redux/store";
 import App from "./App.tsx";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "leaflet/dist/leaflet.css";
-import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter basename="/desafio-just-travel">
+        <App />
+      </BrowserRouter>
     </Provider>
   </StrictMode>
 );
